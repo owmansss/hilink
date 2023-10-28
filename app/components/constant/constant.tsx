b9fc0e3ab8e21b948d204d890f28576226bad350
Suyudi <egiiss17@gmail.com>
2023-10-28 21:15:14 +0700

    modified href4

--- app/components/constant/constant.tsx
+++ app/components/constant/constant.tsx
@@ -1,7 +1,7 @@
 // NAVIGATION
 export const NAV_LINKS = [
-  { href: 'https://www.facebook.com/owmansss', key: 'home', label: 'Home' },
-  { href: '/twitter.com/foosniffer', key: 'how_hilink_work', label: 'How Hilink Work?' },
+  { href: 'facebook.com', key: 'home', label: 'Home' },
+  { href: 'twitter.com', key: 'how_hilink_work', label: 'How Hilink Work?' },
   { href: 'github.com/owmansss', key: 'services', label: 'Services' },
   { href: '/', key: 'pricing ', label: 'Pricing ' },
   { href: '/', key: 'contact_us', label: 'Contact Us' },
